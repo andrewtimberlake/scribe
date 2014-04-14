155571fc777b181b6da1a838dd661301ce36257d
Andrew Timberlake <andrew@andrewtimberlake.com>
2014-04-14 15:39:09 +0200

    Adding block level elements

--- src/plugins/core/formatters/html/enforce-p-elements.js
+++ src/plugins/core/formatters/html/enforce-p-elements.js
@@ -25,7 +25,7 @@ define([
 
 
   // TODO: not exhaustive?
-  var blockElementNames = ['P', 'LI', 'DIV', 'BLOCKQUOTE', 'UL', 'OL', 'H2'];
+  var blockElementNames = ['P', 'LI', 'DIV', 'BLOCKQUOTE', 'UL', 'OL', 'H1','H2', 'H3', 'H4', 'H5', 'H6', 'SECTION'];
   function isBlockElement(node) {
     return contains(blockElementNames, node.nodeName);
   }
